a92bbeb4210d053e5db3e05168e25238322d8301
CloudBasedAaron <aaronradio@gmail.com>
2024-07-10 08:35:46 -0700

    Update chapter_2.py

--- chapter_2.py
+++ chapter_2.py
@@ -1,1 +1,21 @@
+# Learn Python, chapter 2
 
+import math
+
+# Exercise 1
+
+radius = 5
+volume = (4 / 3) * math.pi * (radius ** 3)
+print(volume) # Output: 523.5987755982989
+
+# Exercise 2
+
+x = 42
+should_equal_one = (math.cos(x) ** 2) + (math.sin(x) ** 2)
+print(should_equal_one) # Output: 1.0
+
+# Exercise 3
+
+print(math.e ** 2)
+print(math.pow(math.e, 2))
+print(math.exp(2))
